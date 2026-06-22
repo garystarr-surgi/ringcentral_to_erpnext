e704456c2a25ffbb82bca7b28ef0c152e1737a8b
garystarr-surgi <gary.starr@surgishop.com>
2026-06-22 15:07:30 -0400

    Rename package from ringcentral_to_erpnext to surgishop_crm
    Updated package name and description for the project.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ringcentral_to_erpnext',
+    name='surgishop_crm',
     version='0.0.1',
     description='Connect RingCentral to ERPNext',
     author='SurgiShop',
