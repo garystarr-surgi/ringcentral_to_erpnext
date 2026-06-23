5d14dc7d7f3eeb434db1a4ff167579a1c29e7e7b
garystarr-surgi <gary.starr@surgishop.com>
2026-06-23 11:46:05 -0400

    Refactor installation process with bootstrap method

--- ringcentral_to_erpnext/install.py
+++ ringcentral_to_erpnext/install.py
@@ -1,15 +1,36 @@
 import frappe
 
+_MODULE = "Ringcentral To Erpnext"
+_APP = "ringcentral_to_erpnext"
+_MODULE_KEY = "ringcentral_to_erpnext"   # scrubbed; used as module_app dict key
+_DOCTYPES = ("ringcentral_settings", "ringcentral_agent")
+
 
 def after_install():
-    _sync_doctypes()
+    _bootstrap()
 
 
 def after_migrate():
-    _sync_doctypes()
+    _bootstrap()
 
 
-def _sync_doctypes():
-    for dt in ("ringcentral_settings", "ringcentral_agent"):
-        frappe.reload_doc("ringcentral_to_erpnext", "doctype", dt, force=True)
+def _bootstrap():
+    _ensure_module_def()
+    _register_in_module_app()
+    for dt in _DOCTYPES:
+        frappe.reload_doc(_MODULE_KEY, "doctype", dt, force=True)
     frappe.db.commit()
+
+
+def _ensure_module_def():
+    if not frappe.db.exists("Module Def", _MODULE):
+        frappe.get_doc({
+            "doctype": "Module Def",
+            "module_name": _MODULE,
+            "app_name": _APP,
+        }).insert(ignore_permissions=True)
+        frappe.db.commit()
+
+
+def _register_in_module_app():
+    """
